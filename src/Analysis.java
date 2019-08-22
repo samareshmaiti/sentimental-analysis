6aa29be751976eafc921904d02b9706b2b21c093
samareshmaiti <samaresh02maiti@gmail.com>
2019-08-22 16:41:35 +0530

    implement sentiment analysis

---
+++ src/Analysis.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+
+public class Analysis {
+
+	public static void main(String[] args) throws IOException {
+
+		String text = "here the work culture is good worst";
+		
+		SentimentAnalyzer sentimentAnalyzer = new SentimentAnalyzer();
+		sentimentAnalyzer.initialize();
+		SentimentResult sentimentResult = sentimentAnalyzer.getSentimentResult(text);
+
+		System.out.println("Sentiment Score: " + sentimentResult.getSentimentScore());
+		System.out.println("Sentiment Type: " + sentimentResult.getSentimentType());
+		System.out.println("Very positive: " + sentimentResult.getSentimentClass().getVeryPositive()+"%");
+		System.out.println("Positive: " + sentimentResult.getSentimentClass().getPositive()+"%");
+		System.out.println("Neutral: " + sentimentResult.getSentimentClass().getNeutral()+"%");
+		System.out.println("Negative: " + sentimentResult.getSentimentClass().getNegative()+"%");
+		System.out.println("Very negative: " + sentimentResult.getSentimentClass().getVeryNegative()+"%");
+
+		String[] str = {
+				" ala 187238 owieoiwue",
+				" bubba )(&(*^&%*^%&^% 92837 ",
+				"koko **&*&^ ... ,kkkd"
+		};
+
+		for( String s: str){
+			s.replaceAll("\\d++", "");
+			System.out.println(s.replaceAll("[^\\w]", ""));
+		}
+
+	}
+
+}
